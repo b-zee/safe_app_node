634a5471dad44ae2d6073853a9c2d35e4727d631
Benno Zeeman <bzeeman@live.nl>
2019-01-23 21:43:02 +0100

    chore: few comments

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -16,40 +16,38 @@ use safe_core::ipc::Permission;
 use std::collections::HashMap;
 use std::ffi::CString;
 use std::ffi::CStr;
+use std::os::raw::c_void;
 
 fn app_is_mock(mut cx: FunctionContext) -> JsResult<JsBoolean> {
     Ok(cx.boolean(safe_app::app_is_mock()))
 }
 
-fn app_pub_enc_key(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+fn app_pub_enc_key(mut cx: FunctionContext<'_>) -> JsResult<JsUndefined> {
     let app = cx.argument::<JsArrayBuffer>(0)?;
     let app = cx.borrow(&app, |data| { data.as_slice::<u8>() });
     let app = u64::from_ne_bytes([app[0], app[1], app[2], app[3], app[4], app[5], app[6], app[7]]) as *const App;
 
-    let cxp = Box::into_raw(Box::new(cx)) as *mut std::os::raw::c_void;
-    // let cxp = Box::new(cx);
-    // let cxp = Box::leak(cxp) as *mut std::os::raw::c_void;
+    let cx = Box::new(cx);
+    let cx = Box::into_raw(cx) as *mut c_void;
 
-    dbg!(cxp);
+    dbg!(cx);
 
     unsafe {
-        safe_app::ffi::crypto::app_pub_enc_key(app, cxp, o_cb);
+        safe_app::ffi::crypto::app_pub_enc_key(app, cx, o_cb);
     }
     extern "C" fn o_cb(
-        user_data: *mut std::os::raw::c_void,
+        user_data: *mut c_void,
         error: *const FfiResult,
         public_key_h: safe_app::ffi::object_cache::EncryptPubKeyHandle
     ) {
-        let cx: Box<FunctionContext> = unsafe { Box::from_raw(user_data as *mut _) };
-        // let mut cx = *cx;
+        let mut cx: Box<FunctionContext> = unsafe { Box::from_raw(user_data as *mut _) };
 
         dbg!(user_data);
-        dbg!(cx.len());
+        dbg!(cx.len()); // Sometimes correct, sometimes gibberish
 
-        // dbg!(user_data);
-        // dbg!(public_key_h);
+        // SEGFAULT
+        let f = cx.argument::<JsFunction>(1).unwrap();
 
-        // let f = cx.argument::<JsFunction>(1).unwrap();
         // let arg1 = cx.number(unsafe { (*error).error_code });
         // let arg2: Handle<'_, JsValue> = if unsafe { (*error).description }.is_null() {
         //     JsNull::new().upcast()
@@ -63,8 +61,6 @@ fn app_pub_enc_key(mut cx: FunctionContext) -> JsResult<JsUndefined> {
 
         // let args: Vec<Handle<JsValue>> = vec![ffi_result.upcast()];
 
-        // dbg!(public_key_h);
-
         // f.call(&mut cx, JsNull::new(), vec![JsNull::new()]);
     }
 
@@ -77,12 +73,12 @@ fn test_create_app(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     let app_id = CString::new(app_id).expect("CString::new failed");
 
     let cxp = Box::new(cx);
-    let cxp = Box::into_raw(cxp) as *mut std::os::raw::c_void;
+    let cxp = Box::into_raw(cxp) as *mut c_void;
 
     unsafe {
         safe_app::ffi::test_utils::test_create_app(app_id.as_ptr(), cxp, o_cb);
     }
-    extern "C" fn o_cb(user_data: *mut std::os::raw::c_void, error: *const FfiResult, o_app: *mut App) {
+    extern "C" fn o_cb(user_data: *mut c_void, error: *const FfiResult, o_app: *mut App) {
         let cx: Box<FunctionContext> = unsafe { Box::from_raw(user_data as *mut _) };
         let mut cx = *cx;
 

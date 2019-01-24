9bb3c1d880acb79ee17f876944adcacf7528601d
Benno Zeeman <bzeeman@live.nl>
2019-01-24 22:05:11 +0100

    fix: make app_pub_enc_key a blocking function
    A quick fix to making the app_pub_enc_key function work. The call_1
    function wraps a blocking mechanism.

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -24,63 +24,19 @@ fn app_is_mock(mut cx: FunctionContext) -> JsResult<JsBoolean> {
 
 fn app_pub_enc_key(mut cx: FunctionContext<'_>) -> JsResult<JsUndefined> {
     let app = cx.argument::<JsArrayBuffer>(0)?;
-    let app = cx.borrow(&app, |data| { data.as_slice::<u8>() });
+    let app = cx.borrow(&app, |data| data.as_slice::<u8>());
     let app = u64::from_ne_bytes([app[0], app[1], app[2], app[3], app[4], app[5], app[6], app[7]]) as *const App;
 
-    let fat_ptr = Box::new(cx);
-    let cx = Box::new(fat_ptr);
-    let cx = Box::into_raw(cx) as *mut c_void;
+    use safe_app::ffi::crypto::app_pub_enc_key;
+    use safe_app::ffi::object_cache::EncryptPubKeyHandle;
+    use ffi_utils::test_utils::call_1;
 
-    dbg!(cx);
-
-    unsafe {
-        safe_app::ffi::crypto::app_pub_enc_key(app, cx, o_cb);
-    }
-    extern "C" fn o_cb(
-        user_data: *mut c_void,
-        error: *const FfiResult,
-        public_key_h: safe_app::ffi::object_cache::EncryptPubKeyHandle
-    ) {
-        let mut cx: Box<Box<FunctionContext>> = unsafe { Box::from_raw(user_data as *mut _) };
-
-        dbg!(user_data);
-        dbg!(cx.len()); // Sometimes correct, sometimes gibberish
-
-        // SEGFAULT
-        let f = cx.argument::<JsFunction>(1).unwrap();
-
-        // let arg1 = cx.number(unsafe { (*error).error_code });
-        // let arg2: Handle<'_, JsValue> = if unsafe { (*error).description }.is_null() {
-        //     JsNull::new().upcast()
-        // } else {
-        //     cx.string(unsafe { CStr::from_ptr( (*error).description ) }.to_str().unwrap()).upcast()
-        // };
-
-        // let ffi_result = JsObject::new(&mut cx);
-        // ffi_result.set(&mut cx, "error_code", arg1).unwrap();
-        // ffi_result.set(&mut cx, "description", arg2).unwrap();
-
-        // let args: Vec<Handle<JsValue>> = vec![ffi_result.upcast()];
-
-        // f.call(&mut cx, JsNull::new(), vec![JsNull::new()]);
-    }
-
+    let app_enc_key: EncryptPubKeyHandle = unsafe { call_1(|ud, cb| app_pub_enc_key(app, ud, cb)).unwrap() };
 
     Ok(JsUndefined::new())
 }
 
-fn test_create_app(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let app_id = cx.argument::<JsString>(0)?.value();
-    let app_id = CString::new(app_id).expect("CString::new failed");
-
-    let fat_ptr = Box::new(cx);
-    let cxp = Box::new(fat_ptr);
-    let cxp = Box::into_raw(cxp) as *mut c_void;
-
-    unsafe {
-        safe_app::ffi::test_utils::test_create_app(app_id.as_ptr(), cxp, o_cb);
-    }
-    extern "C" fn o_cb(user_data: *mut c_void, error: *const FfiResult, o_app: *mut App) {
+    extern "C" fn test_create_app_cb(user_data: *mut c_void, error: *const FfiResult, o_app: *mut App) {
         let mut cx: Box<Box<FunctionContext>> = unsafe { Box::from_raw(user_data as *mut _) };
         let mut cx = **cx; // Removing this line causes errors further along
 
@@ -107,6 +63,18 @@ fn test_create_app(mut cx: FunctionContext) -> JsResult<JsUndefined> {
         f.call(&mut cx, JsNull::new(), args);
     }
 
+
+fn test_create_app(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    let app_id = cx.argument::<JsString>(0)?.value();
+    let app_id = CString::new(app_id).expect("CString::new failed");
+
+    let fat_ptr = Box::new(cx);
+    let cxp = Box::new(fat_ptr);
+    let cxp = Box::into_raw(cxp) as *mut c_void;
+
+    unsafe {
+        safe_app::ffi::test_utils::test_create_app(app_id.as_ptr(), cxp, test_create_app_cb);
+    }
     Ok(JsUndefined::new())
 }
 

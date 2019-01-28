fb76e5fbdd123df3699d3f3191ec938932c25bc3
Benno Zeeman <bzeeman@live.nl>
2019-01-28 23:43:48 +0100

    test: Fix layout and use Node.js assert module

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-function assert(b) { if (!b) { throw new Error(); } }
-
+const assert = require('assert');
 const safe = require('./native/index.node');
 
 const APP_NAME = 'app_from_js';
 
 safe.test_create_app(APP_NAME, (err, app) => {
-    assert(err === null);
-    assert(app instanceof ArrayBuffer);
-
-    safe.app_pub_enc_key(app, (err, key) => {
-	assert(key instanceof ArrayBuffer);
-
-	safe.enc_pub_key_get(app, key, (err, key) => {
+	assert(err === null);
+	assert(app instanceof ArrayBuffer);
+	
+	safe.app_pub_enc_key(app, (err, key) => {
 		assert(key instanceof ArrayBuffer);
+	
+		safe.enc_pub_key_get(app, key, (err, key) => {
+			assert(key instanceof ArrayBuffer);
+		});
 	});
-    });
 
 	safe.app_container_name(APP_NAME, (err, name) => {
 		assert(name === 'apps/' + APP_NAME);
